75ea0657cf6e5f633de4900485d453ade2536445
Doggphin <doggphin@gmail.com>
2024-09-14 03:30:59 -0600

    basic graph insertions

--- main.go
+++ main.go
@@ -9,35 +9,38 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type GraphableNumber struct {
-	FieldName string `json:"fieldName"`
-	Value     int    `json:"value"`
-}
+func graphableNumberHandler(graphCollection *GraphCollection) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			return
+		}
 
-func graphableNumberHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err)
-		return
+		var graphableNumberRequest GraphableNumber
+		err = json.Unmarshal(body, &graphableNumberRequest)
+		if err != nil {
+			w.Write([]byte("Error!"))
+			log.Println(err)
+			return
+		}
+
+		PrettyPrint(graphableNumberRequest)
+		graphCollection.InsertValue(graphableNumberRequest.FieldName, graphableNumberRequest.Value)
+
+		w.Write([]byte("Success!"))
 	}
-
-	var data GraphableNumber
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	PrettyPrint(data)
 }
 
 func main() {
 	println("Started")
 
+	var graphCollection GraphCollection
+	graphCollection.Init()
+
 	router := mux.NewRouter().StrictSlash(true)
 
-	router.Methods("POST").Path("/graphable-number").Name("graphableNumberHandler").Handler(LoggerHandler(http.HandlerFunc(graphableNumberHandler), "graphableNumberHandler"))
+	router.Methods("POST").Path("/graphable-number").Name("graphableNumberHandler").Handler(LoggerHandler(http.HandlerFunc(graphableNumberHandler(&graphCollection)), "graphableNumberHandler"))
 
 	log.Fatal(http.ListenAndServe(":3000", router))
-
 }
