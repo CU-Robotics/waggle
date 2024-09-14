30eb2a79f167c75f3f1071a0d6ecec770d5d96cd
effdotsh <ephram@eff.sh>
2024-09-14 15:36:47 -0600

    working move-robot endpoint

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+type ClientData struct {
+	Type string      `json:"type"`
+	Data interface{} `json:"data"`
+}
 
 type GraphableNumber struct {
 	FieldName string `json:"fieldName"`
@@ -13,24 +20,46 @@ type GraphableNumber struct {
 }
 
 func graphableNumberHandler(w http.ResponseWriter, r *http.Request) {
-	// body, err := io.ReadAll(r.Body)
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
-
-	// var data GraphableNumber
-	// err = json.Unmarshal(body, &data)
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
-
-	// PrettyPrint(data)
-	data := map[string]interface{}{
-		"bob": 7,
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		return
 	}
-	updateWSClients(data)
+
+	var data GraphableNumber
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	PrettyPrint(data)
+}
+
+type RobotPosition struct {
+	X float32 `json:"x"`
+	Y float32 `json:"y"`
+}
+
+func setRobotPositionHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	var data RobotPosition
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	clientData := ClientData{
+		Type: "robot_position",
+		Data: data,
+	}
+	updateWSClients(clientData)
 }
 
 func main() {
@@ -39,6 +68,8 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.Methods("POST").Path("/graphable-number").Name("graphableNumberHandler").Handler(LoggerHandler(http.HandlerFunc(graphableNumberHandler), "graphableNumberHandler"))
+	router.Methods("POST").Path("/robot-position").Name("setRobotPositionHandler").Handler(LoggerHandler(http.HandlerFunc(setRobotPositionHandler), "setRobotPositionHandler"))
+
 	router.Methods("GET").Path("/ws").Name("WebSocketStart").Handler(http.HandlerFunc(wsHandler))
 
 	staticDir := "./static/"
