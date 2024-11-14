b143af6f6fbb6ceb77ee87c0e81596552f7c84af
effdotsh <ephram@eff.sh>
2024-11-13 17:19:49 -0700

    home dir handling

--- static/scripts/editor.js
+++ static/scripts/editor.js
@@ -1,57 +1,55 @@
-var currentFolder = "/home"
-
+var currentFolder = "~/";
 
 document.addEventListener("DOMContentLoaded", () => {
-  getFolder(currentFolder)
-  const backButton = document.getElementById("back-button")
-  backButton.addEventListener("click", back)
+  getFolder(currentFolder);
+  const backButton = document.getElementById("back-button");
+  backButton.addEventListener("click", back);
 });
 
 function getFolder(folderPath) {
-  const explorerContainer = document.getElementById("explorer-container")
+  const explorerContainer = document.getElementById("explorer-container");
   var url = "http://localhost:3000/get-folder";
   // Clears out any previous getFolder requests from DOM
-  explorerContainer.innerHTML = ''
+  explorerContainer.innerHTML = "";
   fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ folderPath: folderPath }),
-  }).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-   }).then((data) => {
-      return data.item
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
     })
     .then((data) => {
-      var color = true
+      return data.item;
+    })
+    .then((data) => {
+      var color = true;
       for (let i = 0; i < data.length; i++) {
-        var fileContainer = document.createElement("div")
-        fileContainer.classList.add("fileContainer")
-        fileContainer.id = data[i].filename
+        var fileContainer = document.createElement("div");
+        fileContainer.classList.add("fileContainer");
+        fileContainer.id = data[i].filename;
         // Creates alternating colors
-        if (color == true)
-          fileContainer.style.backgroundColor = "silver"
-        else 
-          fileContainer.style.backgroundColor = "whiteSmoke"
-        color = !color
-        
+        if (color == true) fileContainer.style.backgroundColor = "silver";
+        else fileContainer.style.backgroundColor = "whiteSmoke";
+        color = !color;
+
         if (data[i].isdir) {
-          var dirIcon = document.createElement("img")
-          dirIcon.src = "./folder.png" 
-          dirIcon.style.marginRight = "8px"
-          fileContainer.classList.add("folder")
-          fileContainer.appendChild(dirIcon)
+          var dirIcon = document.createElement("img");
+          dirIcon.src = "./folder.png";
+          dirIcon.style.marginRight = "8px";
+          fileContainer.classList.add("folder");
+          fileContainer.appendChild(dirIcon);
         }
         // Adds the fileName to the fileContainer
-        fileContainer.appendChild(document.createTextNode(data[i].filename))
-        fileContainer.addEventListener("click", handleClick)
+        fileContainer.appendChild(document.createTextNode(data[i].filename));
+        fileContainer.addEventListener("click", handleClick);
         // Add the file container to the explorerContainer
-        explorerContainer.appendChild(fileContainer)
-
+        explorerContainer.appendChild(fileContainer);
       }
     })
     .catch((error) => {
@@ -60,54 +58,60 @@ function getFolder(folderPath) {
 }
 
 function handleClick(event) {
-  var src = event.srcElement
+  var src = event.srcElement;
   if (src.classList.contains("folder")) {
-    currentFolder = currentFolder + "/" + src.id
-    getFolder(currentFolder)
-  }
-  else {
-    console.log("clicked file")
-    loadFile(currentFolder + "/" + src.id)
+    currentFolder = currentFolder + "/" + src.id;
+    getFolder(currentFolder);
+  } else {
+    console.log("clicked file");
+    loadFile(currentFolder + "/" + src.id);
   }
 }
 
 function loadFile(filePath) {
-  const notepadContainer = document.getElementById("notepad-container")
-  url = "http://localhost:3000/get-file"
-  console.log("Loading: ", filePath)
+  const notepadContainer = document.getElementById("notepad-container");
+  url = "http://localhost:3000/get-file";
+  console.log("Loading: ", filePath);
   fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ filePath: filePath }),
-  }).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  }).then((data) => {
-    notepadContainer.innerHTML = ''
-    var fileContents = atob(data.data)
-    console.log(fileContents)
-    notepadContainer.appendChild(document.createTextNode(fileContents))
   })
-  
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      notepadContainer.innerHTML = "";
+      var fileContents = atob(data.data);
+      console.log(fileContents);
+      notepadContainer.appendChild(document.createTextNode(fileContents));
+    });
 }
 
 function back() {
-  pathArray = currentFolder.split("/")
-  currentFolder = ""
+  pathArray = currentFolder.split("/");
+  currentFolder = "";
   // Reconstructs path without the last part of the path
-  for (let i = 0; i < pathArray.length -1; i++) {
-    currentFolder += "/" + pathArray[i]
+  for (let i = 0; i < pathArray.length - 1; i++) {
+    if (pathArray[i] == "") {
+      continue;
+    }
+    currentFolder += "/" + pathArray[i];
   }
-  getFolder(currentFolder)
+  if (currentFolder == "" || currentFolder == "/~") {
+    currentFolder = "~/";
+  }
+  getFolder(currentFolder);
 }
 
 function binaryToText(binaryString) {
   // Ensure binary string is a multiple of 8
-  let text = '';
+  let text = "";
   for (let i = 0; i < binaryString.length; i += 8) {
     // Extract an 8-bit chunk (byte)
     let byte = binaryString.substring(i, i + 8);
