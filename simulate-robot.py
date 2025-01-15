647f6e28043b32d754b5e68a9eb32114468aa330
BryanTurns <95263942+BryanTurns@users.noreply.github.com>
2025-01-14 23:26:54 -0700

    Added basic argparsing to allow fo it to be more of a CLI tool

--- simulate-robot.py
+++ simulate-robot.py
@@ -1,12 +1,30 @@
-import requests, random
+import requests, random, argparse
 from time import sleep
 import random
 
-ITEMS_PER_CHUNK = 1
-CHUNKS_INTERVAL = 0.1
-URL = "http://localhost:3000/batch"
+POINTS_PER_CHUNK = 1
+REQUEST_INTERVAL = 0.1
+URL = "http://localhost:3000"
+PATH = "/batch"
+
+argparser = argparse.ArgumentParser(
+    prog="simulate-robot",
+    description="Simulate robot requests to Go server for testing purposes."
+)
+
+argparser.add_argument("-host", "--host", type=str, help="the host you want to send /batch requests to. The default is http://localhost:3000")
+argparser.add_argument("-i", "--interval", type=float, help="the time in seconds between requests. The default is 0.1 ")
+argparser.add_argument("-n", "--numpoints", type=int, help="the number of data points to send in each request. The default is 1")
 
 def main():
+    args = argparser.parse_args()
+    if args.host:
+        HOST = args.host
+    if args.interval:
+        REQUEST_INTERVAL = args.interval
+    if args.numpoints:
+        POINTS_PER_CHUNK = args.numpoints
+
     while True:
         data = {
             "graphable-numbers": {
@@ -14,11 +32,11 @@ def main():
                 "Pitch": [],
             }
         }
-        for i in range(ITEMS_PER_CHUNK):
+        for i in range(POINTS_PER_CHUNK):
             data["graphable-numbers"]["Yaw"].append(int(random.betavariate(2, 5)*360))
             data["graphable-numbers"]["Pitch"].append(int(random.betavariate(2, 2) * 20 - 10))
-        res = requests.post(URL, json=data)
-        sleep(CHUNKS_INTERVAL)
+        res = requests.post(HOST+PATH, json=data)
+        sleep(REQUEST_INTERVAL)
 
     
     
