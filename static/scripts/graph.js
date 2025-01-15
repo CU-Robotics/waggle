a54493d3eb65c2cb9cc480bceab014fed1608490
BryanTurns <95263942+BryanTurns@users.noreply.github.com>
2025-01-14 19:53:18 -0700

    Unscrewing my branch

--- static/scripts/graph.js
+++ static/scripts/graph.js
@@ -1,6 +1,9 @@
 var chartsByName = {};
+const file_data = [];
+document.addEventListener("");
 async function addDataToGraph(name, number) {
   if (!chartsByName[name]) {
+    // chart no exist lol
     var container = document.getElementById("graphableNumbersContainer");
     var canvas = document.createElement("canvas");
     canvas.height = 50;
@@ -44,29 +47,51 @@ async function addDataToGraph(name, number) {
       chart: chart,
       dataIndex: 1,
     };
+    file_data.push({
+      name: name,
+      values: [],
+      indices: [],
+    });
   } else {
     var chartObj = chartsByName[name];
     var chart = chartObj.chart;
     var index = chartObj.dataIndex;
-    
+
     chart.data.labels.push(index);
     chart.data.datasets[0].data.push(number);
-    
+    file_data[name]["indices"].push(index);
+    file_data[name]["values"].push(number);
+
     if (chart.data.labels.length > 1000) {
       chart.data.labels.shift();
       chart.data.datasets[0].data.shift();
     }
-    
+
     // chart.update();
     chartObj.dataIndex += 1;
   }
 }
 
+function downloadCSVs() {
+  print(createCSVString("yaw"));
+  for (chartName in chartsByName) {
+    print(chartName);
+  }
+}
+
+function createCSVString(name) {
+  CSVString = "index,value\n";
+  for (var i = 0; i < file_data[name]["values"].length; i++) {
+    CSVString +=
+      file_data[name]["indices"][i] + "," + file_data[name]["values"][i] + "\n";
+  }
+  return CSVString;
+}
+
 randomYaw = 0;
 
-
-setInterval(()=>{
-  for (chartName in chartsByName){
+setInterval(() => {
+  for (chartName in chartsByName) {
     chartsByName[chartName].chart.update();
   }
-}, 50)
+}, 50);
