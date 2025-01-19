f9796ba90fdf3ad8f55f03367f2cf62c2d483b43
BryanTurns <95263942+BryanTurns@users.noreply.github.com>
2025-01-19 09:33:10 -0700

    added toggling of web-sockets

--- static/scripts/ws_handler.js
+++ static/scripts/ws_handler.js
@@ -1,65 +1,77 @@
 reqCounter = 0;
+let socket;
+
 document.addEventListener("DOMContentLoaded", () => {
+  const socketsToggle = document.getElementById("toggle-sockets");
+  socketsToggle.addEventListener("input", toggleSocket);
+
   const messagesDiv = document.getElementById("messages");
-  let socket;
-  let lastTimeStamp = 0;
-  const wsUrl = `ws://${window.location.host}/ws`;
-
-  const openSocket = () => {
-    socket = new WebSocket(wsUrl);
-
-    socket.onopen = () => {
-      console.log("WebSocket connection established");
-      socket.bufferedAmount = 0;
-    };
-
-    socket.onmessage = async (event) => {
-      reqCounter += 1;
-      let data = JSON.parse(event.data);
-      if (data.timestamp < lastTimeStamp) {
-        return;
-      }
-      lastTimeStamp = data.timestamp;
-      if (!("type" in data)) {
-        console.log("Mising Type");
-        return;
-      }
-
-      if (data.type == "set_robot_position") {
-        moveRobotIcon(data.data.x, data.data.y);
-      } else if (data.type == "graph_number") {
-        addDataToGraph(data.data.graphName, data.data.value);
-      } else if (data.type == "display_cv_mat") {
-        updateOrCreateImage(
-          data.data.matName,
-          data.data.base64,
-          data.data.flip
-        );
-        console.log(data.data.flip);
-      } else if (data.type == "batch") {
-        for (const k in data.data["cv-mats"]) {
-          updateOrCreateImage(k, data.data["cv-mats"][k], false);
-        }
-
-        // for(var point of data.data['graphable-numbers']){
-        //   addDataToGraph(point.graphName, point.value);
-        // }
-        batchAddPoints(data.data["graphable-numbers"]);
-      } else {
-        console.log(data);
-        console.log(data.type);
-      }
-    };
-
-    socket.onerror = (error) => {
-      console.error("WebSocket error:", error);
-    };
-
-    socket.onclose = () => {
-      console.log("WebSocket connection closed");
-      setTimeout(openSocket, 500); // Try to reopen after 5 seconds
-    };
-  };
 
   openSocket();
 });
+
+function openSocket() {
+  let lastTimeStamp = 0;
+  const wsUrl = `ws://${window.location.host}/ws`;
+
+  socket = new WebSocket(wsUrl);
+
+  socket.onopen = () => {
+    console.log("WebSocket connection established");
+    socket.bufferedAmount = 0;
+  };
+
+  socket.onmessage = async (event) => {
+    reqCounter += 1;
+    let data = JSON.parse(event.data);
+    if (data.timestamp < lastTimeStamp) {
+      return;
+    }
+    lastTimeStamp = data.timestamp;
+    if (!("type" in data)) {
+      console.log("Mising Type");
+      return;
+    }
+
+    if (data.type == "set_robot_position") {
+      moveRobotIcon(data.data.x, data.data.y);
+    } else if (data.type == "graph_number") {
+      addDataToGraph(data.data.graphName, data.data.value);
+    } else if (data.type == "display_cv_mat") {
+      updateOrCreateImage(data.data.matName, data.data.base64, data.data.flip);
+      console.log(data.data.flip);
+    } else if (data.type == "batch") {
+      for (const k in data.data["cv-mats"]) {
+        updateOrCreateImage(k, data.data["cv-mats"][k], false);
+      }
+
+      // for(var point of data.data['graphable-numbers']){
+      //   addDataToGraph(point.graphName, point.value);
+      // }
+      batchAddPoints(data.data["graphable-numbers"]);
+    } else {
+      console.log(data);
+      console.log(data.type);
+    }
+  };
+
+  socket.onerror = (error) => {
+    console.error("WebSocket error:", error);
+  };
+
+  socket.onclose = () => {
+    console.log("WebSocket connection closed");
+    setTimeout(openSocket, 500); // Try to reopen after 5 seconds
+  };
+}
+
+function toggleSocket() {
+  const socketsToggle = document.getElementById("toggle-sockets");
+
+  if (socketsToggle.checked) {
+    openSocket();
+  } else {
+    socket.onclose = () => {};
+    socket.close();
+  }
+}
