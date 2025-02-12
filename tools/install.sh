4badb3778b3edfdc580fddfd511c885f7b53831c
effdotsh <ephram@eff.sh>
2025-02-11 20:32:32 -0700

    added source bashrc

--- tools/install.sh
+++ tools/install.sh
@@ -2,7 +2,9 @@ sudo apt install curl
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 source ~/.bashrc
 nvm install 20
+source ~/.bashrc
 nvm use 20
 cd client
 npm install
 sudo snap install --classic go
+source ~/.bashrc
