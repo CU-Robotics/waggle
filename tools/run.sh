ec0e1bff49432722e325774a6b84557e2f0b9ed8
Infantry Khadas <curobotics@colorado.edu>
2025-02-19 03:13:35 +0000

    run.sh

--- tools/run.sh
+++ tools/run.sh
@@ -1,5 +1,9 @@
-cd client
-npm run build
-cd ..
-go build
+echo "Killing any existing waggle process"
+pkill waggle
+echo "Waggle process killed"
+cd ~/waggle/
+echo "Starting waggle"
+go build -v
+echo "Build complete"
 nohup ./waggle > waggle.log 2>&1 &
+echo "Waggle started"
